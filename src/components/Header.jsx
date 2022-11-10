17526772431e5f9e1f936fac4670f79108c4b2c1
elektron1k <fop.holoborshchyi@gmail.com>
2022-11-11 00:51:21 +0200

    add Footer, Main, Header, env-local, link for API

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <nav className="purple darken-4">
+      <div className="nav-wrapper">
+        <a href="/#" className="brand-logo">
+          React Shop
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
